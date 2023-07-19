50c7fc1ce4e5e9db3154832c17571c95453ebd2f
Wendel <wh68639@gmail.com>
2023-07-19 17:00:20 -0300

    Aprendendo static como "variavel global"

--- bytebank-encapsulado/src/Conta.java
+++ bytebank-encapsulado/src/Conta.java
@@ -3,8 +3,11 @@ public class Conta {
     private int agencia;
     private int numero;
     private Cliente titular ;
+    private static int total;
 
     public Conta(int agencia, int numero) {
+        Conta.total++;
+        System.out.println("O total de contas é: " + total);
         this.agencia = agencia;
         this.numero = numero;
         System.out.println("Conta criada");
@@ -67,5 +70,9 @@ public class Conta {
 
     public Cliente getTitular() {
         return titular;
+    }
+
+    public static int getTotal() {
+        return Conta.total;
     }
 }
--- bytebank-encapsulado/src/TestaValores.java
+++ bytebank-encapsulado/src/TestaValores.java
@@ -1,8 +1,14 @@
 public class TestaValores {
     public static void main(String[] args) {
         Conta conta = new Conta(1337, 24226);
-
+        int total = 0;
         System.out.println("Agencia: " + conta.getAgencia());
+
+        Conta conta2 = new Conta(1337, 24226);
+
+        Conta conta3 = new Conta(1337, 24226);
+
+        System.out.println(Conta.getTotal());
 
     }
 }
