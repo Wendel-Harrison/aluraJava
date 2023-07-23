0432d9ecb2e51ebec27cec03ee412c5766723290
Wendel <wh68639@gmail.com>
2023-07-23 13:11:38 -0300

    começando a estudar herança no Java

---
+++ bytebank-herdado/src/TesteFuncionario.java
@@ -0,0 +1,15 @@
+public class TesteFuncionario {
+    public static void main(String[] args) {
+
+        Funcionario mico = new Funcionario();
+
+        mico.setNome("Mico");
+        mico.setCpf("45685241545");
+        mico.setSalario(2050.00);
+
+        System.out.println(mico.getNome());
+        System.out.println(mico.getBonificacao());
+
+
+    }
+}
