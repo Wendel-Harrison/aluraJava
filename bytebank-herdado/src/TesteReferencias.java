9a51b0ad30b574ae507962157353ce9465d6b5a2
Wendel <wh68639@gmail.com>
2023-07-25 16:37:00 -0300

    Aplicando o polimorfismo mas ainda com dúvidas

--- bytebank-herdado/src/TesteReferencias.java
+++ bytebank-herdado/src/TesteReferencias.java
@@ -1,10 +1,24 @@
 public class TesteReferencias {
     public static void main(String[] args) {
-        Funcionario g1 = new Gerente();
+        Gerente g1 = new Gerente();
 
         g1.setNome("Wendel");
         String nome = g1.getNome();
+        g1.setSalario(5000);
 
+        Funcionario f = new Funcionario();
+        f.setSalario(2000);
+
+        EditorDeVideo e = new EditorDeVideo();
+        e.setSalario(2500);
+
+        ControleBonificacao controle = new ControleBonificacao();
+        controle.registra(g1);
+        controle.registra(f);
+        controle.registra(e);
+
+
+        System.out.println(controle.getSoma());
         System.out.println(nome);
     }
 }
