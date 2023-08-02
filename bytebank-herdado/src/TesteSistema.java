351601d1113acc983047bdc078506e62987d96e7
Wendel <wh68639@gmail.com>
2023-08-02 12:58:42 -0300

    mais uma classe abstrata e novo funcionario

---
+++ bytebank-herdado/src/TesteSistema.java
@@ -0,0 +1,16 @@
+public class TesteSistema {
+    public static void main(String[] args) {
+        Gerente g = new Gerente();
+
+        Administrador adm = new Administrador();
+        adm.setSenha(3333);
+
+        g.setSenha(2222);
+
+        SistemaInterno sistema = new SistemaInterno();
+        sistema.autentica(g);
+        sistema.autentica(adm);
+
+
+    }
+}
