17b65c1c93264a04cc1d8e2181d207f62926fe40
Wendel <wh68639@gmail.com>
2023-08-18 13:04:54 -0300

    trabalhando com exceções e melhorando o código com exceções checked

---
+++ bytebank-herdado-conta/src/TesteSaca.java
@@ -0,0 +1,22 @@
+public class TesteSaca  {
+    public static void main(String[] args) {
+        ContaCorrente cc = new ContaCorrente(22,223);
+
+        cc.deposita(200);
+
+
+
+        try {
+            cc.saca(300);
+            System.out.println("Saque bem sucedido!");
+        }
+        catch (SaldoInsuficienteException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println("Saque mal sucedido!");
+            System.out.println();
+        }
+
+        System.out.println("Seu Saldo é: " + cc.saldo);
+
+    }
+}
