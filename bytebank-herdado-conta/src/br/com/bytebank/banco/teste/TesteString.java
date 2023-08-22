a67de3cbb98627a32cbe53e34518fd4ad71b4f83
Wendel <wh68639@gmail.com>
2023-08-22 14:46:11 -0300

    aprendendo alguns metódos da classe String

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -9,8 +9,17 @@ public class TesteString {
 
         //String outro = new String("Alura");
 
+//        char c = nome.charAt(2);
+        int i = nome.indexOf("ur");
+        String sub = nome.substring(1);
+
         String nome2 = nome.replace("A", "a");
 
         System.out.println(nome2);
+        System.out.println(i);
+        System.out.println(sub);
+        System.out.println(nome.contains("Alu"));
+
+
     }
 }
